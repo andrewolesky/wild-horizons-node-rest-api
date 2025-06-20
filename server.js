fcf08fbd3d49c6b1ae2a917df0251c7b11581371
Andrew Olesky <oleskyinbox@gmail.com>
2025-06-20 17:04:40 -0400

    Set up CORS.

--- server.js
+++ server.js
@@ -3,17 +3,21 @@ import http from "node:http";
 import { getDataFromDB } from "./database/db.js";
 import { sendJSONResponse } from "./utils/sendJSONResponse.js";
 import { getDataByPathParams } from "./utils/getDataByPathParams.js";
+import { getDataByQueryParams } from "./utils/getDataByQueryParams.js";
 
 const PORT = 8000;
 
-const destinations = await getDataFromDB();
-
 const server = http.createServer(async (req, res) => {
-  // console.log(req.url);
-  // res.write("This is some data \n");
-  // res.write("This is some more data \n");
-  if (req.url === "/api" && req.method === "GET") {
-    sendJSONResponse(res, 200, destinations);
+  const destinations = await getDataFromDB();
+
+  const urlObj = new URL(req.url, `http://${req.headers.host}`);
+
+  const queryObj = Object.fromEntries(urlObj.searchParams);
+
+  if (urlObj.pathname === "/api" && req.method === "GET") {
+    let filteredData = getDataByQueryParams(destinations, queryObj);
+
+    sendJSONResponse(res, 200, filteredData);
   } else if (req.url.startsWith("/api/continent") && req.method === "GET") {
     const continent = req.url.split("/").pop();
     const filteredData = getDataByPathParams(
