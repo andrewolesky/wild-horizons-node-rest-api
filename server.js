6cb5ec9d41c7f9aeb6cf0d1c1903bc5f4b93bc2e
Andrew Olesky <oleskyinbox@gmail.com>
2025-06-20 15:45:08 -0400

    Refactored the code by adding some utility functions.

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import http from "node:http";
 // import { createServer } from "node:http";
 import { getDataFromDB } from "./database/db.js";
+import { sendJSONResponse } from "./utils/sendJSONResponse.js";
+import { getDataByPathParams } from "./utils/getDataByPathParams.js";
 
 const PORT = 8000;
 
@@ -11,26 +13,25 @@ const server = http.createServer(async (req, res) => {
   // res.write("This is some data \n");
   // res.write("This is some more data \n");
   if (req.url === "/api" && req.method === "GET") {
-    res.setHeader("Content-Type", "application/json");
-    res.statusCode = 200;
-    res.end(JSON.stringify(destinations));
+    sendJSONResponse(res, 200, destinations);
   } else if (req.url.startsWith("/api/continent") && req.method === "GET") {
     const continent = req.url.split("/").pop();
-    const filteredData = destinations.filter((destination) => {
-      return destination.continent.toLowerCase() === continent.toLowerCase();
-    });
-    res.setHeader("Content-Type", "application/json");
-    res.statusCode = 200;
-    res.end(JSON.stringify(filteredData));
+    const filteredData = getDataByPathParams(
+      destinations,
+      "continent",
+      continent
+    );
+    sendJSONResponse(res, 200, filteredData);
+  } else if (req.url.startsWith("/api/country") && req.method === "GET") {
+    const country = req.url.split("/").pop();
+    const filteredData = getDataByPathParams(destinations, "country", country);
+    sendJSONResponse(res, 200, filteredData);
   } else {
     res.setHeader("Content-Type", "application/json");
-    res.statusCode = 404;
-    res.end(
-      JSON.stringify({
-        error: "not found",
-        message: "The requested route does not exist",
-      })
-    );
+    sendJSONResponse(res, 404, {
+      error: "not found",
+      message: "The requested route does not exist",
+    });
   }
 });
 
